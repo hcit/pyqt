ad2f77af57505ed974341c2a252c4f440be02b57
Timur <timur@lanko-desktop.(none)>
2013-03-30 14:34:42 +0200

    SwitchViews updated, but not working

--- SwitchViews.py
+++ SwitchViews.py
@@ -3,7 +3,7 @@
 import sys
 from PyQt4 import QtGui, QtCore
 
-class UI( QtGui.QMainWindow ):
+class SwitchViews( QtGui.QMainWindow ):
 	def __init__( self ):
 		super( self.__class__, self ).__init__()
 		self.build()
@@ -27,9 +27,18 @@ class UIElement:
 	def build( self ):
 		pass
 
+class UIWidget( QtGui.QWidget ):
+	def __init__( self, master ):
+		super( UIWidget, self ).__init__()
+		self.master = master
+		self.build()
+	
+	def build( self ):
+		pass
 
 
-class Action:
+
+class Action( UIElement ):
 	def build( self ):
 		self.exitAction = QtGui.QAction( QtGui.QIcon( 'exit.png' ), '&Exit', self.master )        
 		self.exitAction.setShortcut( 'Ctrl+Q' )
@@ -39,12 +48,12 @@ class Action:
 		self.listViewAction = QtGui.QAction( QtGui.QIcon( 'exit.png' ), '&List', self.master )        
 		self.listViewAction.setShortcut( 'Ctrl+L' )
 		self.listViewAction.setStatusTip( 'List Files' )
-		self.listViewAction.triggered.connect( self.master.Action.listViewActionCallback )
+		self.listViewAction.triggered.connect( self.listViewActionCallback )
 		
 		self.textViewAction = QtGui.QAction( QtGui.QIcon( 'exit.png' ), '&Text', self.master )        
 		self.textViewAction.setShortcut( 'Ctrl+T' )
 		self.textViewAction.setStatusTip( 'Text Editor' )
-		self.textViewAction.triggered.connect( self.master.Action.textViewActionCallback )
+		self.textViewAction.triggered.connect( self.textViewActionCallback )
 		
 		self.master.statusBar()
 	
@@ -56,7 +65,7 @@ class Action:
 
 
 
-class Control:
+class Control( UIElement ):
 	def build( self ):
 		self.menuControl = self.master.menuBar()
 		self.fileMenuControl = self.menuControl.addMenu( '&File' )
@@ -67,27 +76,64 @@ class Control:
 
 
 
-class View:
+class View( UIWidget ):
 	__views = {}
-	widget = None
 	
 	def build( self ):
-		self.widget = QtGui.QWidget( self.master )
-		layout = QtGui.QBoxLayout()
+		layout = QtGui.QHBoxLayout()
 		layout.addStretch( 1 )
-		self.widget.setLayout( layout )
+		self.setLayout( layout )
+		self.master.setCentralWidget( self )
 	
 	def display( self, viewName ):
+		
 		if viewName == 'list':
 			if not 'list' in self.__views.keys():
-				self.__views['list'] = QtGui.QWidget( self.widget )
-				
+				self.__views['list'] = ViewListView( self )
+		elif viewName == 'text':
+			if not 'text' in self.__views.keys():
+				self.__views['text'] = ViewListView( self )
+		
+		for key in self.__views.keys():
+			if key == viewName:
+				self.__views[key].show()
+			else:
+				self.__views[key].hide()
+
+class ViewListView( View ):
+	def build( self ):
+		layout = QtGui.QVBoxLayout( self )
+		layout.addStretch( 1 )
+		self.setLayout( layout )
+		leftWidget = self.leftWidget()
+		rightWidget = self.rightWidget()
+		splitter = QtGui.QSplitter( QtCore.Qt.Horizontal, self.master )
+		splitter.addWidget( leftWidget )
+		splitter.addWidget( rightWidget )
+		#self.addWidget( splitter )
+	
+	def leftWidget( self ):
+		widget = QtGui.QWidget( self.master )
+		widget.setStyleSheet('color: green')
+		layout = QtGui.QVBoxLayout( widget )
+		layout.addStretch( 1 )
+		widget.setLayout( layout )
+		return widget
+	
+	def rightWidget( self ):
+		widget = QtGui.QWidget( self.master )
+		widget.setStyleSheet('color: red')
+		layout = QtGui.QVBoxLayout( widget )
+		layout.addStretch( 1 )
+		widget.setLayout( layout )
+		return widget
+	
 
 
 
 def main():
-	app = QtGui.QtApplication()
-	ui = UI()
+	app = QtGui.QApplication( sys.argv )
+	ui = SwitchViews()
 	sys.exit( app.exec_() )
 
 if __name__ == "__main__":
